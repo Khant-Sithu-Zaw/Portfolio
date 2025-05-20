87d16ba7277e849bfda4ada75c1af4ed6b58404a
Khant Sithu Zaw <98268896+Khant-Sithu-Zaw@users.noreply.github.com>
2025-05-20 13:52:19 +0630

    add exp data

--- constant/experiences.tsx
+++ constant/experiences.tsx
@@ -1,21 +1,30 @@
 export const experiences = [
-    {
-      company: "METATEAM MYANMAR Co.,Ltd",
-      role: "Junior Web Developer",
-      location: "Myanmar",
-      date: "Jan 2022 - Dec 2022",
-      description: `Converted designs from Photoshop, Figma, and Adobe XD into responsive web pages using pure CSS and Angular. Ensured pixel-perfect layout alignment, optimized for different screen sizes and browsers. Collaborated closely with UI/UX designers to deliver clean and user-friendly interfaces.`,
-      skills: ["HTML5", "CSS3", "SCSS", "Responsive Design", "JavaScript", "Angular", "jQuery"],
-      logoSrc: "/MTM.png",
-    },
-    {
-      company: "Brycen Myanmar Co.,Ltd",
-      role: "IT Engineer",
-      location: "Myanmar",
-      date: "May 2024 - April 2025",
-      description: `Working on Java-based enterprise systems and Visual Basic macros for Japanese clients. Write unit and integration test cases in Japanese, debug code, and handle documentation. Also responsible for developing Python scripts using AWS services such as S3, Athena, and Glue for data processing automation.`,
-      skills: ["VBA", "Java", "Postgresql", "Oracle","AWS S3+Athena","Unit Testing","Integration testing","Python Script+AWS Glue"],
-      logoSrc: "/brycen.png",
-    
-    },
-  ];
+  {
+    company: "METATEAM MYANMAR Co.,Ltd",
+    role: "Junior Web Developer",
+    location: "Myanmar",
+    date: "Jan 2022 - Dec 2022",
+    description: `Converted designs from Photoshop, Figma, and Adobe XD into responsive web pages using pure CSS and Angular. Ensured pixel-perfect layout alignment, optimized for different screen sizes and browsers. Collaborated closely with UI/UX designers to deliver clean and user-friendly interfaces.`,
+    skills: ["HTML5", "CSS3", "SCSS", "Responsive Design", "JavaScript", "Angular", "jQuery"],
+    logoSrc: "/MTM.png",
+  },
+  {
+    company: "CGM Golden Land Co.,Ltd(AOTS研修)",
+    role: "Full-Stack Developer Trainee",
+    location: "Myanmar",
+    date: "Nov 2023 - Mar 2024",
+    description: `Completed a 4-month training program focused on full-stack development. Gained hands-on experience with frontend and backend technologies, including Java, Spring Boot, and MySQL. Also participated in a 1-month Business Manner and Speaking Practice session with native Japanese in order to work effectively in a Japanese corporate environment.`,
+    skills: ["Frontend", "Spring Boot", "Business Manner"],
+    logoSrc: "/aots.png",
+  },
+  {
+    company: "Brycen Myanmar Co.,Ltd",
+    role: "IT Engineer",
+    location: "Myanmar",
+    date: "May 2024 - April 2025",
+    description: `Working on Java-based enterprise systems and Visual Basic macros for Japanese clients. Write unit and integration test cases in Japanese, debug code, and handle documentation. Also responsible for developing Python scripts using AWS services such as S3, Athena, and Glue for data processing automation.`,
+    skills: ["VBA", "Java", "Postgresql", "Oracle", "AWS S3+Athena", "Unit Testing", "Integration testing", "Python Script+AWS Glue"],
+    logoSrc: "/brycen.png",
+
+  },
+];
